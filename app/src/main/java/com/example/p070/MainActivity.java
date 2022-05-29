a3e057498f47e6fe249acd65e5430a45df117c22
User <ostashkovladislav@yandex.ru>
2022-05-29 20:00:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/p070/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.p070;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // TODO Auto-generated method stub
+        // добавляем пункты меню
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO Auto-generated method stub
+        Intent intent =null;
+        switch (item.getItemId())
+        {
+            case R.id.m1: intent = new Intent(this, theatre.class);
+                break;
+            case R.id.m2: intent = new Intent(this, films.class);
+                break;
+            case R.id.m3: intent = new Intent(this, newtheatre.class);
+                break;
+            case R.id.m4: intent = new Intent(this, newcinema.class);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        startActivity(intent);
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+}
